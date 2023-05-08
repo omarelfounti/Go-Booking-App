bc5c639e0b5a2a1d603b2fbb85cca1ecf5df8f03
Omar El Founti Khsim <omarelf@k-d557-mac-16.pk.amk.root>
2023-05-08 14:23:28 +0300

    statemets added

--- Booking-App.go
+++ Booking-App.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+    )
 
 func main() {
 	conferenceName := "Go Conference"
@@ -14,7 +17,8 @@ func main() {
 	fmt-Printf("we have total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
 	fmt.println("Get your tickets here to attend")
 
-	
+	for {
+
 	var firstName string
 	var lastName string
 	var emailAdress string
@@ -36,20 +40,46 @@ func main() {
 	fmt.Println("enter the number of tickets: ")
 	fmt.Scan(&numberOfTickets)
 
+	isValidName := len(firstName) >= 2 && len(lastName) >= 2 
+	isValidEmail := strings.Contains(email, "@")
+    isValidTicketNumber := userTickets > 0 && userTickets <= remainingTickets
 
-	remainingTickets = remainingTickets - userTickets
-	bookings = append(bookings, firstName + " " + lastName)
+    if isValidName && isValidEmail && isValidTicketNumber{
+		remainingTickets = remainingTickets - userTickets
+	    bookings = append(bookings, firstName + " " + lastName)
+		fmt.Printf("Thank you %v %v for buying %v tickets. You will receive a confirmation in your email %v\n.", firstName, lastName, numberofTickets, emailAdress)
+	    fmt.println("%v tickets remaining for %v\n", remainingTickets, conferenceName)
 
-	fmt.Printf("the whole array: %v\n.", bookings)
-	fmt.Printf("the first value: %v\n.", bookings[0])
-	fmt.Printf("Array type: %v\n.", bookings)
-	fmt.Printf("Array length: %v\n.", len(bookings))
+        firstNames := []string{}
+	    for index, booking := range bookings {
+		var names = strings.Fields(booking)
+		firstNames = append(firstNames, names[0])
+ 	   }
+		
+	} 
+	fmt.Printf("The first names of bookings are: %v\n", firstNames)
 
-	fmt.Printf("Thank you %v %v for buying %v tickets. You will receive a confirmation in your email %v\n.", firstName, lastName, numberofTickets, emailAdress)
-	fmt.println("%v tickets remaining for %v\n", remainingTickets, conferenceName)
+    
+	if remainingTickets == 0 {
+		fmt.Println("conference is booked out.")
+		break
+	}else {
+		if !isValidName{
+			fmt.Println("first name or las name you entered is too short")
+		}
+		if !isValidEmail{
+			fmt.Println("email address you entered doesn't contain @ sign")
+		}
+		if !isValidTicketNumber{
+			fmt.Println("number of tickets you entered is invalid")
+		}
+        fmt.Printf("you input data is invalid. Try again")
+	  }
 
-	fmt.Println("These are the bookings from the application: %v\n.", bookings)
 
+   }
 
+	
+	
 }
 
